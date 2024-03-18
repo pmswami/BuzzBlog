8661e7c88936021c05859d4003c8ac474a8ec116
unknown <usern5949@gmail.com>
2024-03-18 12:00:06 +0530

    Added blueprints

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -3,23 +3,36 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+from flaskblog.config import Config
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "TEST_SECRET"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-db = SQLAlchemy(app)
+app.config.from_object(Config)
+db = SQLAlchemy()
 app.app_context().push()
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = "users.login"
 login_manager.login_message_category = "info"
 
-app.config["MAIL_SERVER"] = "smtp.googlemail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+
 mail = Mail(app)
 
-from flaskblog import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    from flaskblog.users.routes import users
+
+    app.register_blueprint(users)
+    from flaskblog.posts.routes import posts
+
+    app.register_blueprint(posts)
+    from flaskblog.main.routes import main
+
+    app.register_blueprint(main)
+    return app
--- Flask_Blog/flaskblog/users/forms.py
+++ Flask_Blog/flaskblog/users/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import (
     DataRequired,
     Length,
@@ -68,12 +68,6 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError(
                     "Email is already taken! Please choose valid email"
                 )
-
-
-class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    content = TextAreaField("Content", validators=[DataRequired()])
-    submit = SubmitField("Post")
 
 
 class RequestResetForm(FlaskForm):
