3a83854c2d47d5958eb3bc3e1c95f40aee323041
unknown <usern5949@gmail.com>
2024-03-18 16:09:45 +0530

    Added error handlers

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -35,4 +35,7 @@ def create_app(config_class=Config):
     from flaskblog.main.routes import main
 
     app.register_blueprint(main)
+    from flaskblog.errors.handlers import errors
+
+    app.register_blueprint(errors)
     return app
